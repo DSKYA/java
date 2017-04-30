b50ac911c22b6feda01945475ea69d72d3f73bf8
DSKYA <chenxiangjun19931227@gmail.com>
2017-04-30 15:52:54 +0800

    15:32

--- 算法实验凸包问题/python/py.py
+++ 算法实验凸包问题/python/py.py
@@ -1,14 +1,33 @@
 import matplotlib.pyplot as plt
-file_object = open('thefile.txt')
+file_gs = open('gs1000.txt')
+file_test = open('test1000.txt')
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+plt.xlabel('X')
+plt.ylabel('Y')
+ax1.set_title('n = 1000')
+
 try:
-     all_the_text = file_object.read( )
+     x = []
+     y = []
+     ox = []
+     oy = []
+     for line in file_test:
+         s = line.split()
+         ox.append(s[0])
+         oy.append(s[1])
+     for line in file_gs:
+         s = line.split()
+         x.append(s[0])
+         y.append(s[1])
+     x.append(x[0])
+     y.append(y[0])
+     ax1.scatter(ox, oy, c='r', marker='o')
+     plt.plot(x, y)
+     plt.show()
+
 finally:
-     file_object.close( )
-input = open('data', 'r')
-list_of_all_the_lines = file_object.readlines( )
-x = []
-y = []
-for line in file_object:
-     s = line.split(" ")
-     x.append(s[0])
-     y.append(s[1])
+     file_gs.close( )
+     file_test.close()
+
+
